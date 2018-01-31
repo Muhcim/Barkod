3fc03ab9b0139de880f739c7d19d6c0ef03b14fe
Muhcim <34978284+Muhcim@users.noreply.github.com>
2018-02-01 00:39:24 +0300

    Barkod

---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CSHARP_Barcode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var MyBarcode = new Barcode();
+            pictureBox1.Image = MyBarcode.Create(500, 100, 15, textBox1.Text);
+            //pictureBox1.Image = MyBarcode.CreateCode128
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
